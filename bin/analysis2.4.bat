e3073f2c2a4c6600ba38b0c09bf3e0c73eda9c8e
Ed Brooksbank <ejb66@le.ac.uk>
2020-02-05 12:39:52 +0000

    added script (.bat) to run from windows

--- bin/analysis2.4.bat
+++ bin/analysis2.4.bat
@@ -1,13 +1,12 @@
 
-setlocal
-pushd..
-set CCPNMR_TOP_DIR=%CD%
-popd
-set ANACONDA3=%CCPNMR_TOP_DIR%\miniconda
-set PYTHONPATH=.;%CCPNMR_TOP_DIR%\ccpnmr2.4\python
-set PATH=%CCPNMR_TOP_DIR%\bin:${PATH}
-set LD_LIBRARY_PATH=%ANACONDA3%\Lib
+@setlocal
+@pushd..
+@set CCPNMR_TOP_DIR=%CD%
+@popd
+@set ANACONDA3=%CCPNMR_TOP_DIR%\miniconda
+@set PYTHONPATH=.;%CCPNMR_TOP_DIR%\ccpnmr2.4\python
+@set PATH=%ANACONDA3%\lib\site-packages\numpy\.libs;%ANACONDA3%;%ANACONDA3%\Library\mingw-w64\bin;%ANACONDA3%\Library\usr\bin;%ANACONDA3%\Library\bin;%ANACONDA3%\Scripts;%ANACONDA3%\bin;%CCPNMR_TOP_DIR%\bin;%PATH%
 
-set ANALYSIS="%CCPNMR_TOP_DIR%"\ccpnmr2.4\python\ccpnmr\analysis\AnalysisGui.py
-"%ANACONDA3%"\python -i -O -W ignore::DeprecationWarning "%ANALYSIS%" $*
-endlocal
+@set ANALYSIS="%CCPNMR_TOP_DIR%"\ccpnmr2.4\python\ccpnmr\analysis\AnalysisGui.py
+@"%ANACONDA3%"\python -i -O -W ignore::DeprecationWarning "%ANALYSIS%" %*
+@endlocal
