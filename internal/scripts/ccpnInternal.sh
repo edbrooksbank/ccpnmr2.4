ed987da6d33b753c893d9190988c55ddf16dd6c5
Ed Brooksbank <ejb66@le.ac.uk>
2020-02-24 18:48:58 +0000

    added nef (nefio) repository

--- internal/scripts/ccpnInternal.sh
+++ internal/scripts/ccpnInternal.sh
@@ -9,7 +9,11 @@ DATA_DIR=""
 INCLUDE_FILES=""
 
 # repositories contained in the project
-REPOSITORY_NAMES=(ccpnmr2.4)
-REPOSITORY_PATHS=(${CCPNMR_TOP_DIR})
-REPOSITORY_RELATIVE_PATHS=("")
-REPOSITORY_SOURCE=(https://github.com/VuisterLab)
+REPOSITORY_NAMES=(ccpnmr2.4
+                  nefio)
+REPOSITORY_PATHS=(${CCPNMR_TOP_DIR}
+                  ${CCPNMR_TOP_DIR}/ccpnmr2.5/python/ccpnmr/nef)
+REPOSITORY_RELATIVE_PATHS=(""
+                           /ccpnmr2.5/python/ccpnmr/nef)
+REPOSITORY_SOURCE=(https://github.com/VuisterLab
+                   git@bitbucket.org:ccpnmr)
