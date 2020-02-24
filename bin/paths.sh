02874c6ab324484df8cb47a9d0e018688a044311
Ed Brooksbank <ejb66@le.ac.uk>
2020-02-24 15:11:37 +0000

    renamed scripts to 2.5 for current version

---
+++ bin/paths.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CCPNMR_TOP_DIR="$(cd "$(dirname "$0")/.." || exit; pwd)"
+export CCPNMR_TOP_DIR
+export VERSION_PATH=ccpnmr2.4
+export ANACONDA3="${CCPNMR_TOP_DIR}"/miniconda
+export PYTHONPATH="${CCPNMR_TOP_DIR}/${VERSION_PATH}/python"
+
+UNAME="$(uname -s)"
+if [[ ${UNAME} == 'Darwin' ]]; then
+  export DYLD_FALLBACK_LIBRARY_PATH=/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/ImageIO.framework/Versions/A/Resources:
+  export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}"${ANACONDA3}"/lib:
+  export DYLD_FALLBACK_LIBRARY_PATH=${DYLD_FALLBACK_LIBRARY_PATH}${HOME}/lib:/usr/local/lib:/usr/lib:
+fi
