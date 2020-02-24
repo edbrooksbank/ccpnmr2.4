ceca225e3181e2290db76628fb4293bbc0c1e361
Ed Brooksbank <ejb66@le.ac.uk>
2020-02-24 15:17:56 +0000

    renamed scripts to 2.5 for current version minor script change to paths.sh

--- internal/scripts/paths.sh
+++ internal/scripts/paths.sh
@@ -3,5 +3,6 @@
 # get the paths for Analysis root from the current path
 CCPNMR_TOP_DIR="$(cd "$(dirname "$0")/../.." || exit; pwd)"
 export CCPNMR_TOP_DIR
-export ANACONDA3=${CCPNMR_TOP_DIR}/miniconda
-export PYTHONPATH=".:${CCPNMR_TOP_DIR}/ccpnmr2.4/python"
+export VERSION_PATH=ccpnmr2.4
+export ANACONDA3="${CCPNMR_TOP_DIR}"/miniconda
+export PYTHONPATH="${CCPNMR_TOP_DIR}/${VERSION_PATH}/python"
