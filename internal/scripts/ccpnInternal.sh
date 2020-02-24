2c333b165c00b6d4817ba3c2b1204e0ad3adbd7b
Ed Brooksbank <ejb66@le.ac.uk>
2020-02-24 17:47:31 +0000

    updated repository name

--- internal/scripts/ccpnInternal.sh
+++ internal/scripts/ccpnInternal.sh
@@ -9,7 +9,7 @@ DATA_DIR=""
 INCLUDE_FILES=""
 
 # repositories contained in the project
-REPOSITORY_NAMES=(ccpnmr)
+REPOSITORY_NAMES=(ccpnmr2.4)
 REPOSITORY_PATHS=(${CCPNMR_TOP_DIR})
 REPOSITORY_RELATIVE_PATHS=("")
 REPOSITORY_SOURCE=(https://github.com/VuisterLab)
